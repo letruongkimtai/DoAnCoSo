572b4180adf0e47240a9de7bbe2e46651308576b
TheITer <letruongkimtai@gmail.com>
2018-05-04 16:02:50 +0700

    Transfer layout

---
+++ DoAn/DoAn/Views/Shared/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Views.Shared
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
