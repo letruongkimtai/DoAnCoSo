62ef5a2fb93f62c9988d43c2f682382c358669c7
Kim Tài <letruongkimtai@gmail.com>
2018-05-25 13:38:23 +0700

    else

--- DoAn/DoAn/Models/Giohang.cs
+++ DoAn/DoAn/Models/Giohang.cs
@@ -8,28 +8,28 @@ namespace DoAn.Models
 {
     public class Giohang
     {
-        //Tao doi tuong data chua dữ liệu từ model dbBansach đã tạo. 
         dbQLMonanDataContext data = new dbQLMonanDataContext();
         public int iMamon{set; get;}
         public string sTenmon{set; get;}
         public int sGiaban{set; get;}
         public int iSoluong { set; get; }
         public string sAnh { set; get; }
-        public string sThucuong { set; get; }
         public Double dDongia{set; get;}
         public Double dThanhtien
         {
-            get { return iSoluong * dDongia; }
+            get { return (iSoluong * dDongia); }
 
         }
-        //Khoi tao gio hàng theo Masach duoc truyen vao voi Soluong mac dinh la 1
         public Giohang(int Mamon)
         {
             iMamon = Mamon;
             CTMONAN monan = data.CTMONANs.Single(n => n.Mamon == iMamon);
             sTenmon = monan.Tenmon;
             sAnh = monan.Anh;
+<<<<<<< HEAD
             //sThucuong = monan.Thucuong;
+=======
+>>>>>>> 9d2926018fcf0fda040851d7e13bca009b90b490
             dDongia =double.Parse(monan.Giaban.ToString());
             iSoluong = 1;
         }
