072ffe117b25f5ed5dab338f7159790034fa3f10
trungminhminh <38715621+trungminhminh@users.noreply.github.com>
2018-05-29 14:26:36 +0700

    admin

--- DoAn/DoAn/Controllers/AdminController.cs
+++ DoAn/DoAn/Controllers/AdminController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DoAn.Models;
 namespace DoAn.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
+        dbQLMonanDataContext db = new dbQLMonanDataContext();
+        // GET: /Admin/
         public ActionResult Index()
         {
             return View();
@@ -16,6 +17,36 @@ namespace DoAn.Controllers
         [HttpGet]
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến 
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                //Gán giá trị cho đối tượng được tạo mới (ad)        
+
+                Admin ad = db.Admins.SingleOrDefault(n => n.UserAdmin == tendn && n.PassAdmin == matkhau);
+                if (ad != null)
+                {
+                    // ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
             return View();
         }
     }
