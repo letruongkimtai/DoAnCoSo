af0b520efe1defa6aa430e60f12c2f6c4c6bbf95
trungminhminh <38715621+trungminhminh@users.noreply.github.com>
2018-05-31 09:14:22 +0700

    admin

--- DoAn/DoAn/Controllers/AdminController.cs
+++ DoAn/DoAn/Controllers/AdminController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DoAn.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
 namespace DoAn.Controllers
 {
     public class AdminController : Controller
@@ -14,6 +17,12 @@ namespace DoAn.Controllers
         {
             return View();
         }
+        public ActionResult Monan(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            return View(db.CTMONANs.ToList().OrderBy(n => n.Mamon).ToPagedList(pageNumber, pageSize));
+        }
         [HttpGet]
         public ActionResult Login()
         {
@@ -22,7 +31,6 @@ namespace DoAn.Controllers
         [HttpPost]
         public ActionResult Login(FormCollection collection)
         {
-            // Gán các giá trị người dùng nhập liệu cho các biến 
             var tendn = collection["username"];
             var matkhau = collection["password"];
             if (String.IsNullOrEmpty(tendn))
@@ -35,12 +43,9 @@ namespace DoAn.Controllers
             }
             else
             {
-                //Gán giá trị cho đối tượng được tạo mới (ad)        
-
                 Admin ad = db.Admins.SingleOrDefault(n => n.UserAdmin == tendn && n.PassAdmin == matkhau);
                 if (ad != null)
                 {
-                    // ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
                     Session["Taikhoanadmin"] = ad;
                     return RedirectToAction("Index", "Admin");
                 }
@@ -48,6 +53,120 @@ namespace DoAn.Controllers
                     ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
             }
             return View();
+        }
+        [HttpGet]
+        public ActionResult ThemmoiMonan()
+        {
+            ViewBag.Maloai = new SelectList(db.LOAIMONs.ToList().OrderBy(n => n.Tenloai), "Maloai", "Tenloai");
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemmoiMonan(CTMONAN monan, HttpPostedFileBase fileUpload)
+        {
+            ViewBag.Maloai = new SelectList(db.LOAIMONs.ToList().OrderBy(n => n.Tenloai), "Maloai", "Tenloai");
+            if (fileUpload == null)
+            {
+                ViewBag.Thongbao = "Vui lòng chọn ảnh bìa";
+                return View();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var fileName = Path.GetFileName(fileUpload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images2"), fileName);
+                    if (System.IO.File.Exists(path))
+                        ViewBag.Thongbao = "Hình ảnh đã tồn tại";
+                    else
+                    {
+                        fileUpload.SaveAs(path);
+                    }
+                    monan.Anh = fileName;
+                    db.CTMONANs.InsertOnSubmit(monan);
+                    db.SubmitChanges();
+                }
+                return RedirectToAction("Monan");
+            }
+        }
+        public ActionResult Chitietmonan(int id)
+        {
+            CTMONAN monan = db.CTMONANs.SingleOrDefault(n => n.Mamon == id);
+            ViewBag.Mamon = monan.Mamon;
+            if (monan == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(monan);
+        }
+        [HttpGet]
+        public ActionResult Xoamonan(int id)
+        {
+            CTMONAN monan = db.CTMONANs.SingleOrDefault(n => n.Mamon == id);
+            ViewBag.Mamon = monan.Mamon;
+            if (monan == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(monan);
+        }
+
+        [HttpPost, ActionName("Xoamonan")]
+        public ActionResult Xacnhanxoa(int id)
+        {
+            CTMONAN monan = db.CTMONANs.SingleOrDefault(n => n.Mamon == id);
+            ViewBag.Mamon = monan.Mamon;
+            if (monan == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.CTMONANs.DeleteOnSubmit(monan);
+            db.SubmitChanges();
+            return RedirectToAction("Monan");
+        }
+        public ActionResult Suamonan(int id)
+        {
+            CTMONAN monan = db.CTMONANs.SingleOrDefault(n => n.Mamon == id);
+            ViewBag.Mamon = monan.Mamon;
+            if (monan == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            ViewBag.Mamon = new SelectList(db.LOAIMONs.ToList().OrderBy(n => n.Tenloai), "Maloai", "Tenloai", monan.Mamon);
+            return View(monan);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Suamonan(CTMONAN monan, HttpPostedFileBase fileUpload)
+        {
+            ViewBag.Maloai = new SelectList(db.LOAIMONs.ToList().OrderBy(n => n.Tenloai), "Maloai", "Tenloai");
+            if (fileUpload == null)
+            {
+                ViewBag.Thongbao = "Vui lòng chọn ảnh bìa";
+                return View();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var fileName = Path.GetFileName(fileUpload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images2"), fileName);
+                    if (System.IO.File.Exists(path))
+                        ViewBag.Thongbao = "Hình ảnh đã tồn tại";
+                    else
+                    {
+                        fileUpload.SaveAs(path);
+                    }
+                    monan.Anh = fileName;
+                    UpdateModel(monan);
+                    db.SubmitChanges();
+                }
+                return RedirectToAction("Monan");
+            }
         }
     }
 }
